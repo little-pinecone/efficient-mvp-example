707b4ca970daec665a46f3a3c7d7030f188f5913
little-pinecone <marta.szymek.mail@gmail.com>
2021-01-06 11:44:06 +0100

    Initial commit

---
+++ src/main/java/in/keepgrowing/efficientmvpexample/EfficientMvpExampleApplication.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.efficientmvpexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfficientMvpExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EfficientMvpExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/in/keepgrowing/efficientmvpexample/EfficientMvpExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.efficientmvpexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfficientMvpExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
