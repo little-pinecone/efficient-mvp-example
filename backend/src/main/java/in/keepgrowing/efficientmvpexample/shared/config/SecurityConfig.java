aa0cefded393ba604bacbb3598b277870862c901
little-pinecone <marta.szymek.mail@gmail.com>
2021-06-14 21:32:37 +0200

    WIP - Disable security on dev environment for open api endpoint, remove the .openapi-generator-ignore file from a FILES list (after running code generation

---
+++ backend/src/main/java/in/keepgrowing/efficientmvpexample/shared/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package in.keepgrowing.efficientmvpexample.shared.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Profile("dev")
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/v3/api-docs");
+    }
+}
