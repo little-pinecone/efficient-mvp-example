ee6c36c9acc4700281fbf874232858394a191530
little-pinecone <marta.szymek.mail@gmail.com>
2021-07-04 15:49:32 +0200

    Fix OpenAPI config for code generation and running Swagger

--- backend/src/main/java/in/keepgrowing/efficientmvpexample/shared/config/openapi/OpenApiConfig.java
+++ backend/src/main/java/in/keepgrowing/efficientmvpexample/shared/config/openapi/OpenApiConfig.java
@@ -1,23 +1,38 @@
 package in.keepgrowing.efficientmvpexample.shared.config.openapi;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
-@OpenAPIDefinition(servers = {
-        @Server(url = "http://localhost:8080")
-})
+@OpenAPIDefinition
+@AllArgsConstructor
 public class OpenApiConfig {
+
+    private static final String CODE_GENERATION_PROFILE = "angular";
+
+    private final Environment environment;
 
     @Bean
     public OpenAPI customOpenAPI(OpenApiProperties properties) {
-        return new OpenAPI()
+        var openApi = new OpenAPI()
                 .info(getInfo(properties));
+
+        if (Arrays.asList(environment.getActiveProfiles()).contains(CODE_GENERATION_PROFILE)) {
+            var server = getLocalhostServer();
+            openApi.setServers(Collections.singletonList(server));
+        }
+
+        return openApi;
     }
 
     private Info getInfo(OpenApiProperties properties) {
@@ -31,5 +46,11 @@ public class OpenApiConfig {
         return new License()
                 .name("Unlicense")
                 .url("https://unlicense.org/");
+    }
+
+    private Server getLocalhostServer() {
+        var server = new Server();
+        server.setUrl("http://localhost:8080");
+        return server;
     }
 }
